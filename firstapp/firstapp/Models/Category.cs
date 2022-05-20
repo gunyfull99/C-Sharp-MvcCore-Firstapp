e62b5b7b8c61439468b6d9a1dc5795df257df237
Nguyễn Minh Hiếu <hieu000502@gmail.com>
2022-05-20 10:41:47 +0700

    1

---
+++ firstapp/firstapp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace firstapp.Models
+{
+    public class Category
+    {   
+        [Key]   
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!!")]
+        public int displayOrder { get; set; }
+        public DateTime createDateTime { get; set; }= DateTime.Now;
+    }
+}
+                
